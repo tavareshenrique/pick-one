8fb4e39095c8b29f55c53445c1c595af62a6f715
Henrique Tavares <ihenrits@gmail.com>
2024-06-25 00:25:52 -0300

    feat: Add genre route for retrieving movie genres
    The commit adds a new file, `route.ts`, under the `src/app/api/movies/genre` directory. This file contains the implementation for retrieving movie genres. It includes an API endpoint that accepts a `GET` request and returns a list of genres. The implementation utilizes the `next/server` package for handling the request and response. This addition enhances the functionality of the application by providing a way to fetch and display movie genres.

---
+++ src/app/api/movies/genre/route.ts
@@ -0,0 +1,127 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+type TGenre = {
+  id: number
+  name: string
+}
+
+export interface IGenreResponse {
+  genres: TGenre[]
+}
+
+const HELP = ['Thriller', 'Terror']
+const SAD = ['Drama', 'Romance']
+const WORRIED = [
+  'Ação',
+  'Crime',
+  'Documentário',
+  'Ficção científica',
+  'Guerra',
+  'Mistério',
+  'Guerra',
+  'Faroeste',
+]
+const NEUTRAL = [
+  'Comédia',
+  'Família',
+  'Fantasia',
+  'Aventura',
+  'Ficção científica',
+]
+const HAPPY = [
+  'Animação',
+  'Comédia',
+  'Família',
+  'Fantasia',
+  'Aventura',
+  'Música',
+]
+const IN_LOVE = ['Romance', 'Comédia']
+const WONDERFUL = ['Aventura', 'Família', 'Fantasia', 'Ação', 'Comédia']
+
+export async function GET(req: NextRequest) {
+  if (req.method !== 'GET') {
+    return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 })
+  }
+
+  const url = 'https://api.themoviedb.org/3/genre/movie/list?language=pt-BR'
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MmVjZWE1NzJkNDZmMmIzNDY1NWVmMmExNjAzZjBmZSIsIm5iZiI6MTcxOTI4MjcwMC44MjM0MDQsInN1YiI6IjVkMGVhNGZlYzNhMzY4MGE2ZjIxMzFiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dlanSIMsJ-OWAp0O2Yr8ZO8nzdhR4e_XLSHCS9c1dPs',
+    },
+  }
+
+  try {
+    const genresResponse = await fetch(url, options)
+
+    const genresData = (await genresResponse.json()) as IGenreResponse
+
+    const searchParams = req.nextUrl.searchParams
+
+    const feeling = searchParams.get('feeling')
+
+    if (!feeling) {
+      return NextResponse.json(
+        { message: 'Feeling query param is required' },
+        { status: 400 },
+      )
+    }
+
+    const genresIds = genresData.genres.reduce((acc, genre) => {
+      if (feeling.toLocaleLowerCase() === 'help' && HELP.includes(genre.name)) {
+        acc.push(genre.id)
+      }
+
+      if (feeling.toLocaleLowerCase() === 'sad' && SAD.includes(genre.name)) {
+        acc.push(genre.id)
+      }
+
+      if (
+        feeling.toLocaleLowerCase() === 'worried' &&
+        WORRIED.includes(genre.name)
+      ) {
+        acc.push(genre.id)
+      }
+
+      if (
+        feeling.toLocaleLowerCase() === 'neutral' &&
+        NEUTRAL.includes(genre.name)
+      ) {
+        acc.push(genre.id)
+      }
+
+      if (
+        feeling.toLocaleLowerCase() === 'happy' &&
+        HAPPY.includes(genre.name)
+      ) {
+        acc.push(genre.id)
+      }
+
+      if (
+        feeling.toLocaleLowerCase() === 'inlove' &&
+        IN_LOVE.includes(genre.name)
+      ) {
+        acc.push(genre.id)
+      }
+
+      if (
+        feeling.toLocaleLowerCase() === 'wonderful' &&
+        WONDERFUL.includes(genre.name)
+      ) {
+        acc.push(genre.id)
+      }
+
+      return acc
+    }, [] as number[])
+
+    return NextResponse.json(genresIds, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 },
+    )
+  }
+}
