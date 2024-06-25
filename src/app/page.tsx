d4ef557c55474e6e3e0b21ce1a4c6e38ac2a276a
Henrique Tavares <ihenrits@gmail.com>
2024-06-24 23:30:20 -0300

    ```text feat: Refactor Home page with Steps component
    The commit refactors the Home page by replacing the existing content with the Steps component. This change improves the structure and organization of the page, making it easier to manage and maintain. The Steps component provides a step-by-step flow for users, enhancing the user experience and guiding them through the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,14 @@
-'use client'
-
-import Image from 'next/image'
-import { useState } from 'react'
-
-import { Slider } from '@/components/ui/slider'
-import { cn } from '@/lib/utils'
-
-const FEELINGS = [
-  {
-    id: 1,
-    name: 'Socorro',
-    image: '/images/feelings/00-help.svg',
-  },
-  {
-    id: 2,
-    name: 'Triste',
-    image: '/images/feelings/01-sad.svg',
-  },
-  {
-    id: 3,
-    name: 'Sei lá',
-    image: '/images/feelings/02-worried.svg',
-  },
-  {
-    id: 4,
-    name: 'Normal',
-    image: '/images/feelings/03-neutral.svg',
-  },
-  {
-    id: 5,
-    name: 'Feliz',
-    image: '/images/feelings/04-happy.svg',
-  },
-  {
-    id: 6,
-    name: 'Apaixonado',
-    image: '/images/feelings/05-in-love.svg',
-  },
-  {
-    id: 7,
-    name: 'Extraordinário',
-    image: '/images/feelings/06-wonderful.svg',
-  },
-]
+import { Steps } from '@/components/steps'
+import { StepsProvider } from '@/context/StepContext'
 
 export default function Home() {
-  const [feelingInterval, setFeelingInterval] = useState([50])
-
-  function getFeelings() {
-    if (feelingInterval[0] <= 10) {
-      return FEELINGS[0]
-    }
-    if (feelingInterval[0] <= 25) {
-      return FEELINGS[1]
-    }
-    if (feelingInterval[0] <= 40) {
-      return FEELINGS[2]
-    }
-    if (feelingInterval[0] <= 50) {
-      return FEELINGS[3]
-    }
-    if (feelingInterval[0] <= 65) {
-      return FEELINGS[4]
-    }
-    if (feelingInterval[0] <= 80) {
-      return FEELINGS[5]
-    }
-
-    return FEELINGS[6]
-  }
-
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <section className="flex flex-col items-center justify-center space-y-4 w-full">
-        <div className="flex flex-col items-center justify-center space-y-4 w-1/2 gap-4">
-          <h1 className="text-3xl font-bold text-center">
-            Como você está se sentindo?
-          </h1>
-
-          <Slider
-            onValueChange={setFeelingInterval}
-            value={feelingInterval}
-            defaultValue={feelingInterval}
-            max={100}
-            step={10}
-            className={cn('w-[60%] mb-6')}
-          />
-        </div>
-
-        <div className="flex items-center justify-center space-x-4 p-2">
-          <Image
-            src={getFeelings().image}
-            alt={getFeelings().name}
-            width={80}
-            height={80}
-          />
-          <p>{getFeelings().name}</p>
-        </div>
-
-        <div className="w-full flex items-center justify-center">
-          <button className="p-4 m-8 text-white bg-blue-500 rounded-lg w-1/4">
-            Avançar
-          </button>
-        </div>
-      </section>
-    </main>
+    <StepsProvider>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <section className="flex flex-col items-center justify-center space-y-4 w-full">
+          <Steps />
+        </section>
+      </main>
+    </StepsProvider>
   )
 }
